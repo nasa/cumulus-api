4fb06f283ff4e0beb4ca138e748f3dbb457f52df
Scisco <scisco7@gmail.com>
2016-08-22 09:26:28 -0400

    linting + read table names from another module

--- src/controllers.js
+++ src/controllers.js
@@ -4,36 +4,33 @@ var _ = require('lodash');
 var dynamoose = require('dynamoose');
 var models = require('./models');
 var utils = require('./utils');
+var tb = require('./tables');
 
-var datasetTableName = process.env.DATASET_TABLE_NAME || 'datasets';
-var granulesTablePrefix = process.env.GRANULES_PREFIX || 'granules_';
-
-module.exports.listDataSets = function(req, cb) {
-
-  var Dataset = dynamoose.model(datasetTableName, models.dataSetSchema, {create: false});
+module.exports.listDataSets = function (req, cb) {
+  var Dataset = dynamoose.model(tb.datasetTableName, models.dataSetSchema, {create: false});
 
   Dataset
         .scan()
         .limit(utils.getLimit(req.query))
         .startAt(utils.startAt('name', 'S', req.query))
         .exec(function (err, datasets) {
-    return cb(err, datasets)
-  });
+          return cb(err, datasets);
+        });
 };
 
-module.exports.getDataSet = function(req, cb) {
-  var Dataset = dynamoose.model(datasetTableName, models.dataSetSchema, {create: false});
+module.exports.getDataSet = function (req, cb) {
+  var Dataset = dynamoose.model(tb.datasetTableName, models.dataSetSchema, {create: false});
 
-  Dataset.get({name: req.path.short_name}, function(err, dataset) {
-    if (!dataset)
+  Dataset.get({name: req.path.short_name}, function (err, dataset) {
+    if (!dataset) {
       err = 'Record was not found';
-    return cb(err, dataset)
+    }
+    return cb(err, dataset);
   });
 };
 
-module.exports.postDataSet = function(req, cb) {
-
-  var Dataset = dynamoose.model(datasetTableName, models.dataSetSchema, {create: false});
+module.exports.postDataSet = function (req, cb) {
+  var Dataset = dynamoose.model(tb.datasetTableName, models.dataSetSchema, {create: false});
 
   if (_.get(req.headers, 'Token', null) === 'thisisatesttoken') {
     var postedRecord = _.get(req, 'body', {});
@@ -41,49 +38,49 @@ module.exports.postDataSet = function(req, cb) {
     newRecord.save(function (err) {
       return cb(err, postedRecord);
     });
-
   } else {
     return cb('Invalid Token', null);
   }
 };
 
-
-module.exports.listGranules = function(req, cb) {
-
-  //Dataset name
-  var tableName = granulesTablePrefix + req.path.dataSet.toLowerCase();
+module.exports.listGranules = function (req, cb) {
+  // Dataset name
+  var tableName = tb.granulesTablePrefix + req.path.dataSet.toLowerCase();
 
   // WWLLN granules
   var Granules = dynamoose.model(tableName, models.granuleSchema, {create: false, waitForActive: false});
 
-  var scan = Granules.scan()
+  Granules.scan()
           .limit(utils.getLimit(req.query))
           .startAt(utils.startAt('name', 'S', req.query))
           .exec(function (err, records) {
-    if (err) {
-      if (err.message == 'Cannot do operations on a non-existent table') {
-        return cb(`Requested dataset (${req.path.dataSet}) doesn\'t exist`)
-      } else {
-        return cb(err.message, null)
-      }
-    }
-    return cb(null, records)
-  });
+            if (err) {
+              if (err.message === 'Cannot do operations on a non-existent table') {
+                return cb(`Requested dataset (${req.path.dataSet}) doesn\'t exist`);
+              } else {
+                return cb(err.message, null);
+              }
+            }
+            return cb(null, records);
+          });
 };
 
-module.exports.getGranules = function(req, cb) {
-
-  //Dataset name
-  var tableName = granulesTablePrefix + req.path.dataSet.toLowerCase();
+module.exports.getGranules = function (req, cb) {
+  // Dataset name
+  var tableName = tb.granulesTablePrefix + req.path.dataSet.toLowerCase();
 
   // WWLLN granules
   var Granules = dynamoose.model(tableName, models.granuleSchema, {create: false, waitForActive: false});
 
-  Granules.get({name: req.path.granuleName}, function(err, dataset) {
-    if (err)
+  Granules.get({name: req.path.granuleName}, function (err, dataset) {
+    if (err) {
       return cb(err.message, null);
-    if (!dataset)
+    }
+
+    if (!dataset) {
       err = 'Record was not found';
-    return cb(err, dataset)
+    }
+
+    return cb(err, dataset);
   });
 };
