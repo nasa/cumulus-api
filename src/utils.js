6802332c572c846b584e5497f1d90cf97623c0e3
Scisco <scisco7@gmail.com>
2016-08-25 18:26:14 -0400

    linting

--- src/utils.js
+++ src/utils.js
@@ -1,41 +1,38 @@
 var _ = require('lodash');
 
-
 // returns token if present in the Authorization section of header
-var getToken = function(header) {
-  var has_token = new RegExp(/^Token ([^\s]*)$/)
-  var authorization = _.get(header, 'Authorization', null)
+var getToken = function (header) {
+  var authorization = _.get(header, 'Authorization', null);
   var token = authorization.match(/^Token ([^\s]*)$/);
   if (token) {
-      return token[1]
+    return token[1];
   }
 
-  return null
-}
+  return null;
+};
 
-var getLimit = function(query) {
+var getLimit = function (query) {
   return _.toInteger(_.get(query, 'limit', 100));
-}
+};
 
-var startAt = function(field, type, query) {
-  var start = _.get(query, 'start_at', null)
+var startAt = function (field, type, query) {
+  var start = _.get(query, 'start_at', null);
 
-  var obj = {}
-  obj[field] = {}
-  obj[field][type] = start
+  var obj = {};
+  obj[field] = {};
+  obj[field][type] = start;
 
   if (start) {
     return obj;
   } else {
-    return null
+    return null;
   }
-}
-
+};
 
 // Converts an AWS datapipline template to the format
 // accepted by aws-sdk
 var pipelineTemplateConverter = function (arr, fieldName) {
-  var newTemplate = []
+  var newTemplate = [];
 
   arr.map(function (obj) {
     var newObj = {};
@@ -52,18 +49,17 @@ var pipelineTemplateConverter = function (arr, fieldName) {
           field.refValue = value.ref;
         } else if (_.isArray(value)) {
           field.stringValue = value[0];
-        }
-        else if (_.isString(value)) {
+        } else if (_.isString(value)) {
           field.stringValue = value;
         }
-        newObj[fieldName].push(field)
+        newObj[fieldName].push(field);
       }
     });
 
     newTemplate.push(newObj);
   });
 
-  return newTemplate
+  return newTemplate;
 };
 
 module.exports.getToken = getToken;
