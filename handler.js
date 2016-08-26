bc3fb72ce3bcd9a5756974ad731f5606127c5661
Scisco <scisco7@gmail.com>
2016-08-26 13:09:40 -0400

    add trigger as a handler

--- handler.js
+++ handler.js
@@ -1,34 +1,38 @@
 'use strict';
 
 var action = require('./src/controllers');
+var trigger = require('./src/triggers');
 
-module.exports.listDataSets = function(event, context, cb) {
+module.exports.listDataSets = function (event, context, cb) {
   action.listDataSets(event, function (err, results) {
-    return cb(err, results)
-  })
+    return cb(err, results);
+  });
 };
 
-module.exports.getDataSet = function(event, context, cb) {
+module.exports.getDataSet = function (event, context, cb) {
   action.getDataSet(event, function (err, results) {
-    return cb(err, results)
-  })
+    return cb(err, results);
+  });
 };
 
-module.exports.postDataSet = function(event, context, cb) {
+module.exports.postDataSet = function (event, context, cb) {
   action.postDataSet(event, function (err, results) {
-    return cb(err, results)
-  })
+    return cb(err, results);
+  });
 };
 
-
-module.exports.listGranules = function(event, context, cb) {
+module.exports.listGranules = function (event, context, cb) {
   action.listGranules(event, function (err, results) {
-    return cb(err, results)
-  })
+    return cb(err, results);
+  });
 };
 
-module.exports.getGranules = function(event, context, cb) {
+module.exports.getGranules = function (event, context, cb) {
   action.getGranules(event, function (err, results) {
-    return cb(err, results)
-  })
+    return cb(err, results);
+  });
+};
+
+module.exports.trigger = function (event, context, cb) {
+  trigger(cb);
 };
