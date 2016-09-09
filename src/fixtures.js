4af0f345e095c286618524c7d8253a941ab5ed0b
Scisco <scisco7@gmail.com>
2016-09-09 10:49:46 -0400

    load wwlln and avaps

--- src/fixtures.js
+++ src/fixtures.js
@@ -3,17 +3,25 @@
 var dynamoose = require('dynamoose');
 var tables = require('./tables');
 var models = require('./models');
+var avaps = require('./pipeline/avaps');
 var wwlln = require('./pipeline/wwlln');
 
-var populateDataSets = function () {
+var populateDataSets = function (record) {
   var Dataset = dynamoose.model(
     tables.datasetTableName,
     models.dataSetSchema,
     {create: true}
   );
 
-  var newRecord = new Dataset(wwlln);
-  newRecord.save();
+  var newRecord = new Dataset(record);
+  newRecord.save(function (err) {
+    if (err) {
+      return console.error('There is an error saving the record', err);
+    }
+
+    console.log(`${record.name} saved!`);
+  });
 };
 
-populateDataSets();
+populateDataSets(wwlln);
+populateDataSets(avaps);
