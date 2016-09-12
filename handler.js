8a329ce9edb0b979281242267e779c556dd7eb90
Scisco <scisco7@gmail.com>
2016-09-12 09:59:27 -0400

    use named trigger function

--- handler.js
+++ handler.js
@@ -2,7 +2,7 @@
 
 require('dotenv').config({silent: true});
 var action = require('./src/controllers');
-var trigger = require('./src/triggers');
+var triggers = require('./src/triggers');
 
 module.exports.listDataSets = function (event, context, cb) {
   action.listDataSets(event, function (err, results) {
@@ -24,6 +24,7 @@ module.exports.postDataSet = function (event, context, cb) {
 
 module.exports.listGranules = function (event, context, cb) {
   action.listGranules(event, function (err, results) {
+    console.log(JSON.stringify(results));
     return cb(err, results);
   });
 };
@@ -35,5 +36,19 @@ module.exports.getGranules = function (event, context, cb) {
 };
 
 module.exports.trigger = function (event, context, cb) {
-  trigger(cb);
+  triggers.trigger(event.dataset, cb);
 };
+
+// triggers.trigger('WWLLN', function (err, results) {
+//   console.log(err);
+//   console.log(results);
+// });
+
+// action.listGranules({
+//         path: {
+//           dataSet: 'WWLLN'
+//         }
+//       }, function (err, results) {
+//   console.log(JSON.stringify(results));
+//   // return cb(err, results);
+// });
