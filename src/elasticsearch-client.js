f6ef6a7a484b467a0942d3ed6324c9a1f833b4a1
Scisco <scisco7@gmail.com>
2016-09-24 17:57:34 -0400

    use localhost if ES_HOST points to a local address

--- src/elasticsearch-client.js
+++ src/elasticsearch-client.js
@@ -9,12 +9,17 @@ var esHost = process.env.ES_HOST;
 var url = `https://${esHost}`;
 
 // Must have AWS credentials present in environment for this signing function to work
-module.exports = new es.Client({
-  host: url,
-  connectionClass: awsES,
-  amazonES: {
-    region: process.env.AWS_REGION || 'us-east-1',
-    accessKey: process.env.AWS_ACCESS_KEY_ID,
-    secretKey: process.env.AWS_SECRET_ACCESS_KEY
-  }
-});
+
+if (esHost === 'localhost') {
+  module.exports = new es.Client({});
+} else {
+  module.exports = new es.Client({
+    host: url,
+    connectionClass: awsES,
+    amazonES: {
+      region: process.env.AWS_REGION || 'us-east-1',
+      accessKey: process.env.AWS_ACCESS_KEY_ID,
+      secretKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
+  });
+}
