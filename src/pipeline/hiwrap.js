be9c08c5b54a7acb4b1817a4fa2ec681144aae2b
Scisco <scisco7@gmail.com>
2016-09-25 20:02:30 -0400

    add more fixtures

---
+++ src/pipeline/hiwrap.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var Builder = require('./builder');
+
+var recipe = {
+  resource: 'group',
+  name: 'WorkerGroup',
+  steps: [{
+    type: 'archive',
+    name: 'Fetch',
+    action: 'download'
+  }, {
+    type: 'runner',
+    name: 'Process',
+    image: '985962406024.dkr.ecr.us-east-1.amazonaws.com/cumulus-hs3-hiwrap:latest',
+    after: 'Fetch'
+  }, {
+    type: 'metadata',
+    name: 'Metadata',
+    after: 'Process'
+  }, {
+    type: 'archive',
+    name: 'Fetch',
+    action: 'download',
+    after: 'Metadata'
+  }, {
+    type: 'cleanup'
+  }]
+};
+
+var builder = new Builder(recipe);
+
+var datasetRecord = {
+  name: 'hiwrap',
+  shortName: 'hs3hiwrap',
+  versionId: 1,
+  daacName: 'Global Hydrology Resource Center DAAC',
+  sourceDataBucket: {
+    bucketName: 'cumulus-ghrc-raw',
+    prefix: 'hiwrap/',
+    granulesFiles: 1,
+    format: '.nc'
+  },
+  destinationDataBucket: {
+    bucketName: 'cumulus-ghrc-archive',
+    prefix: 'hs3hiwrap/',
+    granulesFiles: 1,
+    format: '.nc'
+  },
+  dataPipeLine: {
+    template: builder.template,
+    parameters: builder.parameters,
+    batchLimit: 10
+  }
+};
+
+module.exports = datasetRecord;
