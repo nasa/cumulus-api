095b9f95b57903fbe2304884e7db303e62b7bb6e
Scisco <scisco7@gmail.com>
2017-01-04 17:44:27 -0500

    payload schema with tests
    closes https://github.com/cumulus-nasa/cumulus-factory/issues/14

--- test/schemas.js
+++ test/schemas.js
@@ -2,11 +2,12 @@
 
 import { validate } from 'jsonschema';
 import assert from 'assert';
-import { collection, granule, invoke, pdr } from '../lib/schemas';
+import { collection, granule, invoke, pdr, payload } from '../lib/schemas';
 import collectionRecord from './data/collection.json';
 import granuleRecord from './data/granule.json';
 import invokeRecord from './data/invoke.json';
 import pdrRecord from './data/pdr.json';
+import payloadRecord from './data/payload.json';
 
 describe('Database Schema validators', () => {
   it('collection table record', () => {
@@ -23,5 +24,9 @@ describe('Database Schema validators', () => {
 
   it('pdr table record', () => {
     assert.ok(validate(pdrRecord, pdr).valid);
+  });
+
+  it('payload object', () => {
+    assert.ok(validate(payloadRecord, payload).valid);
   });
 });
