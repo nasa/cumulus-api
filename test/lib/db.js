04225ca993f6bd49b0f33b737f7b6f90c039e2ef
Derek Lieu <dl@dereklieu.com>
2017-01-27 14:38:28 -0500

    Fix tests

--- test/lib/db.js
+++ test/lib/db.js
@@ -1,7 +1,7 @@
 'use strict';
 import assert from 'assert';
 import sinon from 'sinon';
-import { __forceNewInstance__, get, update } from '../../lib/db';
+import { __forceNewInstance__, get, update, buildUpdateExpression } from '../../lib/db';
 
 const AWS = require('aws-sdk');
 
@@ -24,9 +24,9 @@ describe('db', function () {
 
   it('sets params on #get', function (done) {
     const stub = stubDb({
-      getItem: (params, cb) => cb(null, params)
+      get: (params, cb) => cb(null, params)
     });
-    sinon.stub(AWS, 'DynamoDB', stub);
+    sinon.stub(AWS.DynamoDB, 'DocumentClient', stub);
     const params = {
       key: 'key',
       value: 'value',
@@ -38,17 +38,17 @@ describe('db', function () {
         Key: { [params.key]: params.value },
         TableName: params.table
       });
-      AWS.DynamoDB.restore();
+      AWS.DynamoDB.DocumentClient.restore();
       done();
     });
   });
 
   it('creates update expressions on #update', function (done) {
     const stub = stubDb({
-      getItem: (params, cb) => cb(null, true),
-      updateItem: (params, cb) => cb(null, params)
+      get: (params, cb) => cb(null, true),
+      update: (params, cb) => cb(null, params)
     });
-    sinon.stub(AWS, 'DynamoDB', stub);
+    sinon.stub(AWS.DynamoDB, 'DocumentClient', stub);
     const params = {
       key: 'key',
       value: 'value',
@@ -59,9 +59,27 @@ describe('db', function () {
     };
     update(params, (error, res) => {
       assert.strictEqual(error, null);
-      assert.equal(res.UpdateExpression, 'SET a = b');
-      AWS.DynamoDB.restore();
+      assert.equal(res.UpdateExpression, 'SET #0 = :0');
+      AWS.DynamoDB.DocumentClient.restore();
       done();
     });
+  });
+
+  it('creates update expressions', function (done) {
+    let updates = {
+      foo: 'bar',
+      x: {a: 1, b: 2}
+    };
+    const output = buildUpdateExpression(updates);
+    assert.equal(output.UpdateExpression, 'SET #0 = :0, #1 = :1');
+    assert.deepEqual(output.ExpressionAttributeNames, {
+      '#0': 'foo',
+      '#1': 'x'
+    });
+    assert.deepEqual(output.ExpressionAttributeValues, {
+      ':0': 'bar',
+      ':1': {a: 1, b: 2}
+    });
+    done();
   });
 });
