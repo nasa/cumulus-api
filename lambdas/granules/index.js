dc0dd35581dae254abcf74dc00de88b3126944b1
Derek Lieu <dl@dereklieu.com>
2017-01-27 17:24:33 -0500

    Refactor granules endpoints

--- lambdas/granules/index.js
+++ lambdas/granules/index.js
@@ -1,55 +1,61 @@
 'use strict';
 
+import { get as safeGet } from 'lodash';
 import { esQuery } from 'cumulus-common/es';
 import { granulesTablePrefix } from 'cumulus-common/tables';
 import { getLimit, getStart } from 'cumulus-common/utils';
 
 export function list(event, context, cb) {
-  // Dataset name
-  const tableName = granulesTablePrefix + event.path.collection.toLowerCase();
+  const collection = safeGet(event, 'path.collection');
+  if (!collection) {
+    return cb('Must supply path.collection');
+  }
+  const tableName = granulesTablePrefix + collection.toLowerCase();
   const limit = getLimit(event.query);
   const start = getStart(event.query);
 
   esQuery({
     query: {
-      match: {
-        _index: tableName
-      }
+      match: { _index: tableName }
     },
     size: limit,
     from: start
-  }, (err, res) => {
-    if (err) return cb(err);
-
-    if (res.length === 0) {
-      return cb(`Requested dataset (${event.path.collection}) doesn\'t exist`);
+  }, (error, res) => {
+    if (error) {
+      return cb(error);
+    } else if (_.isEmpty(res)) {
+      return cb('Requested collection ' + collection + ' not found');
+    } else {
+      return cb(null, res);
     }
-
-    return cb(null, res);
   });
 }
 
 export function get(event, context, cb) {
-  // Dataset name
-  const tableName = granulesTablePrefix + event.path.collection.toLowerCase();
+  const collection = safeGet(event, 'path.collection');
+  const granuleName = safeGet(event, 'path.granuleName');
+  if (!collection || !granuleName) {
+    return cb('Must supply path.collection and path.granuleName');
+  }
+  const tableName = granulesTablePrefix + collection.toLowerCase();
 
   esQuery({
     query: {
       bool: {
         must: [
           { match: { _index: tableName } },
-          { match: { name: event.path.granuleName } }
+          { match: { name: granuleName } }
         ]
       }
     }
-  }, (err, res) => {
-    if (err) return cb(err);
-
-    // Cannot have more than 1 document, because `name` is the primary Dynamo key
-    if (res.length === 0) {
+  }, (error, res) => {
+    if (error) {
+      return cb(error);
+    } else if (_.isEmpty(res)) {
+      // Cannot have more than 1 document, because `name` is the primary Dynamo key
       return cb('Record was not found');
+    } else {
+      return cb(null, res[0]);
     }
-
-    return cb(null, res[0]);
   });
 }
