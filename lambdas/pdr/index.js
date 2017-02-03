4886074ec3424f559d66b4e1e715e04625d52d8c
Scisco <scisco7@gmail.com>
2017-02-03 13:39:24 -0500

    update jsdocs

--- lambdas/pdr/index.js
+++ lambdas/pdr/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import { query, sendMessage, putItem } from 'cumulus-common/aws-helpers';
-import { localRun, loadCredentials } from 'cumulus-common/local';
+import { localRun } from 'cumulus-common/local';
 import { syncUrl, downloadS3Files, s3 } from 'gitc-common/aws';
 import Crawler from 'simplecrawler';
 import url from 'url';
@@ -10,12 +10,13 @@ import steed from 'steed';
 import pvl from 'pvl';
 import fs from 'fs';
 
-/*
-* discovers PDR names with a given url and returns
-* a list of PDRs
-* @params {string} url url to the folder where PDRs are listed:
-* @params {function} cb callback function that receives the PDR list
-*/
+/**
+ * discovers PDR names with a given url and returns
+ * a list of PDRs
+ * @param {string} endpoint url to the folder where PDRs are listed:
+ * @param {discoverPDRsCallback} cb
+ * @return {undefined}
+ */
 function discoverPDRs(endpoint, cb) {
   const pattern = /<a href="(PDN.*)">/;
   const c = new Crawler(endpoint);
@@ -47,12 +48,17 @@ function discoverPDRs(endpoint, cb) {
   c.start();
 }
 
-/*
-* checks if PDR lists are synced with DB
-* returns a list of new and existing PDRs
-* @param [array] pdrs An array of PDR strings
-* @return [object] returns a list of PDRs under new and existing keys
-*/
+/**
+ * checks if PDR lists are synced with DB
+ * returns a list of new and existing PDRs
+ * @param {Array<string>} pdrs An array of PDR strings
+ * @return {pdrStatus} returns a list of PDRs under new and existing keys
+ * @example
+ * syncPdrsWithDb(['pdr1', 'pdr2'])
+ * // returns
+ * { new: ['pdr1'],
+ *   existing: ['pdr2']}
+ */
 async function syncPdrsWithDb(pdrs) {
   const result = {
     new: [],
@@ -82,10 +88,11 @@ async function syncPdrsWithDb(pdrs) {
   return result;
 }
 
-/*
-* uploads a given list of PDR Urls to the correct S3 bucket
-* @param [array] pdrs list of pdrs urls
-*/
+/**
+ * uploads a given list of PDR Urls to the correct S3 bucket
+ * @param {Array<pdrStatus>} pdrs list of pdrs urls
+ * @return {Promise} promise, to be resolved on success or rejected on failure
+ */
 function uploadPdrs(pdrs) {
   return new Promise((resolve, reject) => {
     // we download the PDRs in series to avoid
@@ -99,25 +106,24 @@ function uploadPdrs(pdrs) {
         // providers
         log.info(`Adding ${pdr.name} to PDR queue`);
         sendMessage(pdr, 'testQueue1').then(() => {
-          cb();
+          // add the PDR to the table
+          const params = {
+            Item: {
+              pdrName: {
+                S: pdr.name
+              },
+              originalUrl: {
+                S: pdr.url
+              },
+              s3Uri: {
+                S: `s3://cumulus-internal/pdrs/${pdr.name}`
+              }
+            },
+            TableName: process.env.PDRsTable
+          };
+
+          putItem(params, cb);
         });
-
-        // add the PDR to the table
-        const params = {
-          Item: {
-            pdrName: {
-              S: pdr.name
-            },
-            originalUrl: {
-              S: pdr.url
-            },
-            s3Uri: {
-              S: `s3://cumulus-internal/pdrs/${pdr.name}`
-            },
-          },
-          TableName: process.env.PDRsTable
-        }
-
       });
     }, (err) => {
       if (err) {
@@ -132,11 +138,12 @@ function uploadPdrs(pdrs) {
   });
 }
 
-/*
-* This function parse a PDR stored on S3 and download all the files
-* in the PDR to a staging S3 bucket
-* @param [string] pdrName name of the PDR on s3. The PDR must be on cumulus-internal/pdrs folder
-*/
+/**
+ * This async function parse a PDR stored on S3 and download all the files
+ * in the PDR to a staging S3 bucket. The return is void
+ * @param {string} pdrName name of the PDR on s3. The PDR must be on cumulus-internal/pdrs folder
+ * @return {undefined}
+ */
 async function parsePdr(pdrName) {
   try {
     const s3Client = s3();
@@ -191,12 +198,19 @@ async function parsePdr(pdrName) {
 //export function parsePdrsHandler(event, context, cb) {
 
 
-/*
-* Handler for Discovering and Syncing latest PDRs
-* The handler is invoked by a Lambda function.
-* The event object (payload) must include the endpoint for the
-* function to check
-*/
+/**
+ * Handler for discovering and syncing latest PDRs.
+ * The handler is invoked by a Lambda function.
+ *
+ * The event object (payload) must include the endpoint for the
+ * function to check
+ * @param {pdrHandlerEvent} event AWS Lambda uses this parameter to
+ * pass in event data to the handler
+ * @param {object} context
+ * {@link http://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html|AWS Lambda's context object}
+ * @param {function} cb {@link http://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-handler.html#nodejs-prog-model-handler-callback|AWS Lambda's callback}
+ * @return {undefined}
+ */
 export function discoverPdrHandler(event, context, cb) {
   // get the endpoint from event
   const endpoint = event.endpoint;
@@ -227,3 +241,18 @@ localRun(() => {
 
   //parsePdr('PDN.ID1611071200.PDR').then(() => console.log('done'));
 });
+
+/**
+ * @callback discoverPDRsCallback
+ * @param {Array<object>} list an array (list) of PDRs
+ * @example
+ * // Example of list
+ * [{ name: 'pdr1', url: 'example.com' },
+ *  { name: 'pdr2', url: 'example.com' }]
+ */
+
+/**
+ * @typedef {Object} pdrStatus
+ * @property {Array<string>} new list of new pdrs
+ * @property {Array<string>} existing list of existing pdrs
+ */
