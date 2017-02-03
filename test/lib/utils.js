c00a2529513e4e0ccc3f5fe337e9eb5f3d642851
Derek Lieu <dl@dereklieu.com>
2017-02-03 17:08:56 -0500

    Remove config reading function from lambda code

--- test/lib/utils.js
+++ test/lib/utils.js
@@ -1,18 +1,3 @@
 'use strict';
 import assert from 'assert';
-import { hashToId } from '../../lib/utils';
-
-describe('utils', function () {
-  describe('hashToId', function () {
-    it('identifies table names', function (done) {
-      assert.throws(() => hashToId('notARealTable'), /No dynamodb table config exists for ES_TYPE/);
-      const record = {
-        collectionName: 'foo',
-        granuleId: 'bar'
-      };
-      const result = hashToId('x-GranulesTable', record);
-      assert.equal(result, 'foo|bar');
-      done();
-    });
-  });
-});
+describe('utils', function () {});
