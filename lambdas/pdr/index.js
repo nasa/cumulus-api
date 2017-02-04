d19870bf7dd11f69c0209ab27800f7dcbc80ea4c
Scisco <scisco7@gmail.com>
2017-02-03 19:15:15 -0500

    more dev work

--- lambdas/pdr/index.js
+++ lambdas/pdr/index.js
@@ -1,23 +1,23 @@
 'use strict';
 
-import { query, sendMessage, putItem } from 'cumulus-common/aws-helpers';
+import { sendMessage, receiveMessage, putItem } from 'cumulus-common/aws-helpers';
 import { localRun } from 'cumulus-common/local';
-import { syncUrl, downloadS3Files, s3 } from 'gitc-common/aws';
+import { syncUrl, downloadS3Files, fileNotFound } from 'gitc-common/aws';
 import Crawler from 'simplecrawler';
 import url from 'url';
 import log from 'gitc-common/log';
-import steed from 'steed';
 import pvl from 'pvl';
+import path from 'path';
 import fs from 'fs';
+
 
 /**
  * discovers PDR names with a given url and returns
  * a list of PDRs
  * @param {string} endpoint url to the folder where PDRs are listed:
- * @param {discoverPDRsCallback} cb
- * @return {undefined}
+ * @return {Promise} on success the promise includes a list {@link pdrObject|pdrObjects}
  */
-function discoverPDRs(endpoint, cb) {
+function discoverPDRs(endpoint) {
   const pattern = /<a href="(PDN.*)">/;
   const c = new Crawler(endpoint);
 
@@ -27,116 +27,90 @@ function discoverPDRs(endpoint, cb) {
   c.userAgent = 'Cumulus';
   c.maxDepth = 1;
 
-  c.on('fetchcomplete', (queueItem, responseBuffer) => {
-    log.info(`Received the list of PDRs from ${endpoint}`);
-    const lines = responseBuffer.toString().trim().split('\n');
-    const list = [];
-    for (const line of lines) {
-      const split = line.trim().split(pattern);
-      if (split.length === 3) {
-        const name = split[1];
-        list.push({
-          name: name,
-          url: url.resolve(endpoint, name)
-        });
-      }
-    }
-
-    cb(list);
-  });
-
-  c.start();
-}
-
-/**
- * checks if PDR lists are synced with DB
- * returns a list of new and existing PDRs
- * @param {Array<string>} pdrs An array of PDR strings
- * @return {pdrStatus} returns a list of PDRs under new and existing keys
- * @example
- * syncPdrsWithDb(['pdr1', 'pdr2'])
- * // returns
- * { new: ['pdr1'],
- *   existing: ['pdr2']}
- */
-async function syncPdrsWithDb(pdrs) {
-  const result = {
-    new: [],
-    existing: []
-  };
-
-  for (const pdr of pdrs) {
-    const r = await query({
-      TableName: 'PDRsTable',
-      KeyConditionExpression: 'pdrName = :name',
-      ExpressionAttributeValues: {
-        ':name': {
-          S: pdr.name
+  return new Promise((resolve) => {
+    c.on('fetchcomplete', (queueItem, responseBuffer) => {
+      log.info(`Received the list of PDRs from ${endpoint}`);
+      const lines = responseBuffer.toString().trim().split('\n');
+      const list = [];
+      for (const line of lines) {
+        const split = line.trim().split(pattern);
+        if (split.length === 3) {
+          const name = split[1];
+          list.push({
+            name: name,
+            url: url.resolve(endpoint, name)
+          });
         }
       }
+
+      resolve(list);
     });
 
-    if (r.Count === 0) {
-      result.new.push(pdr);
-    }
-    else {
-      result.existing.push(pdr);
-    }
-  }
-
-  log.info('Checked PDRs against the database to find new ones');
-  return result;
-}
-
-/**
- * uploads a given list of PDR Urls to the correct S3 bucket
- * @param {Array<pdrStatus>} pdrs list of pdrs urls
- * @return {Promise} promise, to be resolved on success or rejected on failure
- */
-function uploadPdrs(pdrs) {
-  return new Promise((resolve, reject) => {
-    // we download the PDRs in series to avoid
-    // DDOSing DAACs uncomment next line if want to do parallel
-    steed.each(pdrs, (pdr, cb) => {
-    //steed.eachSeries(pdrs, (pdr, cb) => {
-      log.info(`Uploading ${pdr.name} to S3`);
-      syncUrl(pdr.url, 'cumulus-internal', `pdrs/${pdr.name}`).then(() => {
-        // add the pdr to the queue
-        // (we use queue here because we want to avoid DDOSing
-        // providers
-        log.info(`Adding ${pdr.name} to PDR queue`);
-        sendMessage(pdr, 'testQueue1').then(() => {
-          // add the PDR to the table
-          const params = {
-            Item: {
-              pdrName: {
-                S: pdr.name
-              },
-              originalUrl: {
-                S: pdr.url
-              },
-              s3Uri: {
-                S: `s3://cumulus-internal/pdrs/${pdr.name}`
-              }
-            },
-            TableName: process.env.PDRsTable
-          };
-
-          putItem(params, cb);
-        });
-      });
-    }, (err) => {
-      if (err) {
-        log.error(err);
-        reject(err);
-      }
-      else {
-        log.info(`${pdrs.length} PDRs uploaded to S3`);
-        resolve();
-      }
-    });
+    c.start();
   });
 }
+
+
+/**
+ * Uploads a file from a given URL if file is not found on S3
+ *
+ * @param {string} originalUrl URL of the file that has to be uploaded
+ * @param {string} bucket AWS S3 bucket name
+ * @param {string} fileName name of the file
+ * @param {string} [key=''] s3 folder name(s)
+ * @returns {boolean} true if uploaded / false if not
+ */
+async function uploadIfNotFound(originalUrl, bucket, fileName, key = '') {
+  const fullKey = path.join(key, fileName);
+  // check if the file is already uploaded to S3
+  const notFound = await fileNotFound(bucket, fileName, key);
+  if (notFound) {
+    log.info(`Uploading ${fileName} to S3`);
+    await syncUrl(originalUrl, bucket, fullKey);
+    log.info(`${fileName} uploaded`);
+    return true;
+  }
+  log.info(`${fileName} is already ingested`);
+  return false;
+}
+
+
+/**
+ * uploads a given PDR to S3, adds to DynamoDb and sends it to SQS
+ * @param {pdrObject} pdr pdr object
+ * @return {undefined}
+ */
+async function uploadPdr(pdr) {
+  log.info(`Uploading ${pdr.name} to S3`);
+  await syncUrl(pdr.url, process.env.internal, `pdrs/${pdr.name}`);
+
+  // add the pdr to the queue
+  // (we use queue here because we want to avoid DDOSing
+  // providers
+  log.info(`Adding ${pdr.name} to PDR queue`);
+  await sendMessage(pdr, process.env.PDRsQueue);
+
+  // add the PDR to the table
+  const params = {
+    Item: {
+      pdrName: {
+        S: pdr.name
+      },
+      originalUrl: {
+        S: pdr.url
+      },
+      s3Uri: {
+        S: `s3://${process.env.internal}/pdrs/${pdr.name}`
+      }
+    },
+    TableName: process.env.PDRsTable
+  };
+
+  // saving the item
+  await putItem(params);
+  log.info(`Saved ${pdr.name} to PDRsTable`);
+}
+
 
 /**
  * This async function parse a PDR stored on S3 and download all the files
@@ -146,10 +120,9 @@ function uploadPdrs(pdrs) {
  */
 async function parsePdr(pdrName) {
   try {
-    const s3Client = s3();
     // first download the PDR
     await downloadS3Files(
-      [{ Bucket: 'cumulus-internal', Key: `pdrs/${pdrName}` }],
+      [{ Bucket: process.env.internal, Key: `pdrs/${pdrName}` }],
       '.'
     );
 
@@ -172,21 +145,8 @@ async function parsePdr(pdrName) {
         const fileUrl = url.resolve('https://e4ftl01.cr.usgs.gov:40521/', `${directoryId}/${fileId}`);
 
         // check if the file is already uploaded to S3
-        try {
-          await s3Client.headObject({
-            Key: `staging/${fileId}`,
-            Bucket: 'cumulus-internal'
-          }).promise();
-          log.info(`${fileId} is already ingested`);
-        }
-        catch (e) {
-          // if file is not found download it
-          if (e.stack.match(/(NotFound)/)) {
-            log.info(`Uploading ${fileId} to S3`);
-            await syncUrl(fileUrl, 'cumulus-internal', `staging/${fileId}`);
-            log.info(`${fileId} uploaded`);
-          }
-        }
+        // if not upload it
+        await uploadIfNotFound(fileUrl, process.env.internal, fileId, 'staging');
       }
     }
   }
@@ -195,7 +155,25 @@ async function parsePdr(pdrName) {
   }
 }
 
-//export function parsePdrsHandler(event, context, cb) {
+
+/**
+ * Polls a SQS queue for PDRs that have to be parsed.
+ * The function is recursive and calls itself for ever
+ *
+ */
+function pollPdrQueue() {
+}
+
+/**
+ * Handler for starting a SQS poll for PDRs
+ * This function runs pollPdrQueue indefinitely until cancelled
+ *
+ * @param {object} event AWS Lambda uses this parameter to
+ * pass in event data to the handler
+ * @return {undefined}
+ */
+export function parsePdrsHandler(event) {
+}
 
 
 /**
@@ -211,48 +189,55 @@ async function parsePdr(pdrName) {
  * @param {function} cb {@link http://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-handler.html#nodejs-prog-model-handler-callback|AWS Lambda's callback}
  * @return {undefined}
  */
-export function discoverPdrHandler(event, context, cb) {
+export async function discoverPdrHandler(event, context, cb) {
   // get the endpoint from event
   const endpoint = event.endpoint;
 
-  // first discover the PDRs
-  discoverPDRs(endpoint, (pdrs) => {
-    // find which PDRs are new
-    syncPdrsWithDb(pdrs).then((newPdrs) => {
-      // upload new Pdrs
-      uploadPdrs(newPdrs.new).then(() => {
-        cb();
-      });
-    });
-  });
+  try {
+    // first discover the PDRs
+    const pdrs = await discoverPDRs(endpoint);
+    for (const pdr of pdrs) {
+      // check if PDR is already on S3
+      // if not upload it, add it to DB and send to the queue
+      const uploaded = await uploadIfNotFound(pdr.url, process.env.internal, pdr.name, 'pdrs');
+      if (uploaded) {
+        await uploadPdr(pdr);
+      }
+    }
+    cb(null, `All PDRs ingested on ${Date()} for this endpoint: ${endpoint}`);
+  }
+  catch (e) {
+    cb(e);
+  }
 }
 
-// for local run: node dist/pdr local
+
+// for local run: babel-node lambdas/pdr/index.js local
 localRun(() => {
   // to test discoveringPDRs uncomment here
   //discoverPDRs('https://e4ftl01.cr.usgs.gov:40521/TEST_B/Cumulus/PDR/', (list) => console.log(list));
 
   // discover and upload new PDRs to S3
-  //discoverPdrHandler({ endpoint: 'https://e4ftl01.cr.usgs.gov:40521/TEST_B/Cumulus/PDR/' });
+  //discoverpdrhandler(
+    //{ endpoint: 'https://e4ftl01.cr.usgs.gov:40521/test_b/cumulus/pdr/' },
+    //null,
+    //(err, r) => console.log(err, r)
+  //);
 
-  //const loaded = loadCredentials();
-  //console.log(loaded);
-
+  //uploadPdrs([{
+    //name: 'PDN.ID1611071200.PDR',
+    //url: 'https://e4ftl01.cr.usgs.gov:40521/TEST_B/Cumulus/PDR/PDN.ID1611071200.PDR'
+  //}])
 
   //parsePdr('PDN.ID1611071200.PDR').then(() => console.log('done'));
+
+  receiveMessage(process.env.PDRsQueue).then((d) => console.log(d));
+
 });
 
-/**
- * @callback discoverPDRsCallback
- * @param {Array<object>} list an array (list) of PDRs
- * @example
- * // Example of list
- * [{ name: 'pdr1', url: 'example.com' },
- *  { name: 'pdr2', url: 'example.com' }]
- */
 
 /**
- * @typedef {Object} pdrStatus
- * @property {Array<string>} new list of new pdrs
- * @property {Array<string>} existing list of existing pdrs
+ * @typedef {Object} pdrObject
+ * @property {string} name pdr name
+ * @property {string} url pdr url
  */
