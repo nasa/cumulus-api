7960c56be26309e4904aaf529d2811850417e111
Derek Lieu <dl@dereklieu.com>
2017-02-03 19:10:23 -0500

    Guard against throwing errors

--- lambdas/es/index.js
+++ lambdas/es/index.js
@@ -10,30 +10,30 @@ const type = process.env.ES_TYPE || 'type';
 const hash = process.env.DYNAMODB_HASH || 'no-hash-env';
 const range = process.env.DYNAMODB_RANGE || 'NONE';
 
-export const handler = function(event, context) {
+export const handler = function(event, context, done) {
   console.log(JSON.stringify(event));
   esClient.indices.exists({ index }, (error, response, status) => {
     if (status === 404) {
       esClient.indices.create({ index }, (error, response, status) => {
         if (error) {
-          context.fail(error);
+          done(null, error.message);
         } else {
-          processRecords(event, context);
+          processRecords(event, done);
         }
       });
     } else if (status === 200) {
-      processRecords(event, context);
+      processRecords(event, done);
     } else {
-      context.fail(error);
+      done(null, error.message);
     }
   });
 };
 
-function processRecords (event, context) {
+function processRecords (event, done) {
   const q = queue();
   const records = get(event, 'Records');
   if (!records) {
-    return context.fail('No records found in event');
+    return done(null, 'No records found in event');
   }
   records.forEach((record) => {
     const keys = unwrap(get(record, 'dynamodb.Keys'));
@@ -45,44 +45,48 @@ function processRecords (event, context) {
         q.defer((callback) => deleteRecord(params, callback));
       } else {
         const data = unwrap(record.dynamodb.NewImage);
-        q.defer((callback) => saveRecord(data, params, context, callback));
+        q.defer((callback) => saveRecord(data, params, callback));
       }
     } else {
-      q.defer(() => {
-        throw new Error('Could not find hash value for property name ' + hash);
-      });
+      // defer an error'd callback so we can handle it in awaitAll.
+      q.defer((callback) => callback(new Error('Could not find hash value for property name ' + hash)));
     }
   });
   q.awaitAll((error, result) => {
     if (error) {
-      context.fail(error);
+      done(null, error.message);
     } else {
-      context.succeed('Records altered: ' + result.length);
+      done(null, 'Records altered: ' + result.filter(Boolean).length);
     }
   });
 }
 
 function deleteRecord (params, callback) {
-  esClient.delete(params, function (error, response) {
-    if (error) {
-      throw error;
-    } else {
-      callback(null, response);
+  esClient.get(params, (error, response, status) => {
+    if (status !== 200) {
+      return callback(null, null);
     }
+    esClient.delete(params, function (error, response) {
+      if (error) {
+        callback(error);
+      } else {
+        callback(null, response);
+      }
+    });
   });
 }
 
-function saveRecord (data, params, context, callback) {
+function saveRecord (data, params, callback) {
   esClient.get(params, (error, response, status) => {
     if (status !== 200 && status !== 404) {
-      context.fail(error);
+      callback(error);
     }
     const exists = status === 200;
     const handler = (error, response, status) => {
       if (status === 200 || status === 201) {
         callback(null, data);
       } else {
-        throw error || new Error('Could not write record');
+        callback(error || new Error('Could not write record'));
       }
     };
     if (exists) {
