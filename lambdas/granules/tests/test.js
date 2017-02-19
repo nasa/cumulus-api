61c1dbda6920d30506d412f242022b81fcf60bb7
Scisco <scisco7@gmail.com>
2017-02-19 18:35:09 -0500

    use new es libraries for granules endpoint

--- lambdas/granules/tests/test.js
+++ lambdas/granules/tests/test.js
@@ -1,26 +1,50 @@
 'use strict';
 import assert from 'assert';
-import sinon from 'sinon';
 import record from 'cumulus-common/tests/data/granule.json';
-import * as es from 'cumulus-common/es';
-import { list, get } from '../index';
+import { Search } from 'cumulus-common/es/search';
+import { list } from '../index';
 
 describe('Granules endpoint', () => {
+  before(async () => {
+    // set the env variables
+    process.env.MODE = 'local';
+    process.env.StackName = 'cumulus-api-test-es';
+    process.env.GranulesTable = 'es-test-graneules-table';
+
+    // get ES client
+    const client = Search.es();
+
+    // add index
+    await client.indices.create({ index: process.env.StackName });
+
+    // add record
+    try {
+      await client.index({
+        index: process.env.StackName,
+        type: process.env.GranulesTable,
+        id: record.granuleId,
+        body: record,
+        refresh: true,
+        //consistency: 'all'
+      });
+    }
+    catch (e) {
+      console.error(e);
+    }
+  });
+
   it('lists', (done) => {
-    const spy = sinon.spy((index, table, query, cb) => cb(null, []));
-    sinon.stub(es, 'esQuery', spy);
-    const query = {
-      limit: 10,
-      start_at: 100
-    };
-
-    // accepts no collectionName property
-    assert.doesNotThrow(() => list({}, null, () => true));
-
-    list({ collectionName: 'foo', query }, null, (error, res) => {
-      assert.equal(res.length, 0);
-      es.esQuery.restore();
+    list({}, null, (error, res) => {
+      assert.equal(res.meta.count, 1);
       done();
     });
+  });
+
+  after(async () => {
+    // get ES client
+    const client = Search.es();
+
+    // delete index
+    await client.indices.delete({ index: process.env.StackName });
   });
 });
