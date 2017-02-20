a4dab4fd90cb13cfea53ddc90a641191144d03e9
Scisco <scisco7@gmail.com>
2017-02-20 10:55:56 -0500

    improve cmr pusher

--- lambdas/cmr-push/index.js
+++ lambdas/cmr-push/index.js
@@ -1,71 +1,73 @@
 'use strict';
 
 import aws from 'aws-sdk';
-//import { ingestGranule } from 'cmrjs';
+import Logger from 'cumulus-common/log';
+import { localRun } from 'cumulus-common/local';
+import { ingestGranule } from 'cumulus-common/cmrjs';
+import payloadExample from 'cumulus-common/tests/data/payload.json';
 
+const log = new Logger('lambdas/cmr-push/index.js');
 const s3 = new aws.S3();
 
-const identifyCollection = (payload) => {
+function identifyCollection(payload) {
   return payload.granuleRecord.collectionName;
-};
+}
 
-export const getMetadata = (payload, cb) => {
+
+export async function getMetadata(payload) {
   // Identify the location of the metadata file,
   // conditional on the name of the collection
-  let filepath;
-  const collection = identifyCollection(payload);
-  if (collection === 'AST_L1A') {
-    const xmlFile = Object.keys(payload.granuleRecord.files).find(f => f.endsWith('.xml'));
-    filepath = payload.granuleRecord.files[xmlFile].archivedFile;
-  } else {
-    return cb(new Error("CMR pusher doesn't know how to extract metadata for this collection"));
-  }
+  const filepath = payload.granuleRecord.files['meta-xml'].stagingFile;
 
   // GET the metadata text
   // Currently, only supports files that are stored on S3
   const parts = filepath.match(/^s3:\/\/(.+?)\/(.+)$/);
-  s3.getObject({
+  const obj = await s3.getObject({
     Bucket: parts[1],
     Key: parts[2]
-  }, (err, xml) => cb(err, xml ? xml.Body.toString() : null));
-};
+  }).promise();
 
-export const formatMetadata = (payload, xml) => {
+  log.debug(`Downloaded meta xml file from ${filepath}`);
+
+  return obj.Body.toString();
+}
+
+
+export function formatMetadata(payload, xml) {
   // Format the metadata so that it is compliant with the ECHO-10 XML format
   const collection = identifyCollection(payload);
   if (collection === 'AST_L1A') {
     // ASTER data isn't in complaint ECHO-10 format
     // But for now we'll just pass it through
     return xml;
-  } else {
-    // By default, we'll assume that the xml string is ready to go
-    return xml;
   }
-};
 
-export const postToCMR = (xml, cb) => {
-  //ingestGranule(xml, (err, res) => cb(err, res));
-};
-
-export const handler = (event, context, cb) => {
-  getMetadata(event, (err, xml) => {
-    if (err) { return cb(err); }
-    postToCMR(formatMetadata(xml), (err, res) => cb(err, res));
-  });
-};
+  log.debug('Formatted metdata');
+  // By default, we'll assume that the xml string is ready to go
+  return xml;
+}
 
 
-// To run a small test:
-// babel-node index.js local
-const { setupLocalRun } = require('cumulus-common/local');
+export async function postToCMR(xml) {
+  log.debug('Pushing the metadata to CMR');
+  return await ingestGranule(xml);
+}
 
-setupLocalRun(handler, {
-  granuleRecord: {
-    collectionName: 'AST_L1A',
-    files: {
-      'foo.xml': {
-        archivedFile: 's3://cumulus-internal/testdata/metadata/test-granule.xml'
-      }
-    }
-  }
-}, (e, r) => console.log(e, r));
+
+export function handler(event, context, cb) {
+  log.debug(`Received a new payload: ${JSON.stringify(event)}`);
+  getMetadata(event).then(xml => {
+    const payload = formatMetadata(event, xml);
+    return postToCMR(payload);
+  }).then(res => {
+    log.debug(`successfully posted with this message: ${JSON.stringify(res)}`);
+    cb(null, res);
+  }).catch(e => cb(e));
+
+  // url to check if the CMR is uploaded: https://cmr.uat.earthdata.nasa.gov/search/granules?granule_ur=[granuleUR from xml file]
+}
+
+
+localRun(() => {
+  handler(payloadExample, null, (e, r) => console.log(e, r));
+});
