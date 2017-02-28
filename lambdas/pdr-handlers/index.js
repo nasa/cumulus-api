5bb84631cd0c974403993e9f6db8eb148388e013
Scisco <scisco7@gmail.com>
2017-02-28 04:15:41 -0500

    activate pdr disaggregation

--- lambdas/pdr-handlers/index.js
+++ lambdas/pdr-handlers/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import _ from 'lodash';
+import { localRun } from 'cumulus-common/local';
 import { Search } from 'cumulus-common/es/search';
 
 /**
@@ -12,7 +13,7 @@ import { Search } from 'cumulus-common/es/search';
  */
 export function list(event, context, cb) {
   const search = new Search(event, process.env.PDRsTable);
-  search.query().then((response) => cb(null, response)).catch((e) => {
+  search.query(true).then((response) => cb(null, response)).catch((e) => {
     cb(e);
   });
 }
@@ -30,9 +31,15 @@ export function get(event, context, cb) {
   }
 
   const search = new Search({}, process.env.PDRsTable);
-  search.get(name).then((response) => {
+  search.get(name, true).then((response) => {
     cb(null, response);
   }).catch((e) => {
     cb(e);
   });
 }
+
+localRun(() => {
+  list({}, null, (e, r) => {
+    console.log(e, r);
+  });
+});
