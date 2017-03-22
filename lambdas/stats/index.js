4e5a3793600d4837de24e47d67ddc50147987c7f
Scisco <scisco7@gmail.com>
2017-03-22 19:35:13 -0400

    add count and average to stats

--- lambdas/stats/index.js
+++ lambdas/stats/index.js
@@ -3,8 +3,31 @@
 import _ from 'lodash';
 import moment from 'moment';
 import { handle } from 'cumulus-common/response';
-import { Stats, Histogram } from 'cumulus-common/es/search';
+import { Stats } from 'cumulus-common/es/search';
 import { localRun } from 'cumulus-common/local';
+
+
+function getType(event) {
+  let index;
+
+  const supportedTypes = {
+    granules: process.env.GranulesTable,
+    pdrs: process.env.PDRsTable,
+    collections: process.env.CollectionsTable,
+    logs: null,
+    providers: process.env.ProvidersTable,
+    resources: process.env.ResourcesTable
+  };
+
+  const typeRequested = _.get(event, 'queryStringParameters.type', null);
+  const type = _.get(supportedTypes, typeRequested);
+
+  if (typeRequested === 'logs') {
+    index = `${process.env.StackName}-${process.env.Stage}-logs`;
+  }
+
+  return { type, index };
+}
 
 export function summary(event, cb) {
   let params = _.get(event, 'queryStringParameters', {});
@@ -23,25 +46,24 @@ export function summary(event, cb) {
 }
 
 export function histogram(event, cb) {
-  const supportedTypes = {
-    granules: process.env.GranulesTable,
-    pdrs: process.env.PDRsTable,
-    collections: process.env.CollectionsTable,
-    logs: null,
-    providers: process.env.ProvidersTable,
-    resources: process.env.ResourcesTable
-  };
-  let index;
+  const type = getType(event);
 
-  const typeRequested = _.get(event, 'queryStringParameters.type', null);
-  const type = _.get(supportedTypes, typeRequested);
+  const stats = new Stats(event, type.type, type.index);
+  stats.histogram().then(r => cb(null, r)).catch(e => cb(e));
+}
 
-  if (typeRequested === 'logs') {
-    index = `${process.env.StackName}-${process.env.Stage}-logs`;
-  }
+export function count(event, cb) {
+  const type = getType(event);
 
-  const hist = new Histogram(event, type, index);
-  hist.query().then(r => cb(null, r)).catch(e => cb(e));
+  const stats = new Stats(event, type.type, type.index);
+  stats.count().then(r => cb(null, r)).catch(e => cb(e));
+}
+
+export function average(event, cb) {
+  const type = getType(event);
+
+  const stats = new Stats(event, type.type, type.index);
+  stats.avg().then(r => cb(null, r)).catch(e => cb(e));
 }
 
 export function handler(event, context) {
@@ -52,6 +74,12 @@ export function handler(event, context) {
     else if (event.httpMethod === 'GET' && event.resource === '/stats/histogram') {
       histogram(event, cb);
     }
+    else if (event.httpMethod === 'GET' && event.resource === '/stats/count') {
+      count(event, cb);
+    }
+    else if (event.httpMethod === 'GET' && event.resource === '/stats/average') {
+      average(event, cb);
+    }
     else {
       summary(event, cb);
     }
@@ -60,7 +88,9 @@ export function handler(event, context) {
 
 
 localRun(() => {
-  histogram({ queryStringParameters: {
-    type: 'logs', level: 'error', interval: 'month', format: 'YYYY-MM'
-  }}, (e, r) => console.log(e, r));
+  average({ queryStringParameters:
+  {
+    type: '', field: 'tasks.pendingTasks'
+  }
+  }, (e, r) => console.log(e, r));
 });
