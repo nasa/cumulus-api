b14f2b0d248595004c330a97efcb8ed2171d8941
Scisco <scisco7@gmail.com>
2017-03-22 14:18:33 -0400

    add list endpoint for resources

--- lambdas/resources/index.js
+++ lambdas/resources/index.js
@@ -1,68 +1,14 @@
 'use strict';
 
 import { handle } from 'cumulus-common/response';
+import { Search } from 'cumulus-common/es/search';
+import { localRun } from 'cumulus-common/local';
 
 function list(event, cb) {
-   return cb(null, {
-  "queues": [{
-    "name": "cumulus-api-lpdaac-dev-DispatcherFailedQueue",
-    "messagesAvailable": 2,
-    "messagesInFlight": 0
-  }, {
-    "name": "cumulus-api-lpdaac-dev-GranulesQueue",
-    "messagesAvailable": 2,
-    "messagesInFlight": 0
-  }],
-  "ecsInstances": [{
-    "instanceId": "i-0c3f43f3ab0cbc2c9",
-    "cpuAvailable": 2033,
-    "memoryAvailable": 7474,
-    "runningTasksCount": 2
-  }, {
-    "instanceId": "i-0c3f4234cbc2c9",
-    "cpuAvailable": 1033,
-    "memoryAvailable": 5474,
-    "runningTasksCount": 10
-  }],
-  "tasks": [{
-    "id": "20b71d43-9dda-4778-8af9-b5f463aaf682",
-    "taskDefinition": "cumulus-api-lpdaac-discoverPdrsTaskDefinition-1BW7FAS8ZTPN1:1",
-    "lastStatus": "RUNNING",
-    "desiredStatus": "RUNNING",
-    "startedBy": "N/A"
-  }, {
-    "id": "20b71d43-9dda-4778-8af9-b5f463aaf682",
-    "taskDefinition": "cumulus-api-lpdaac-discoverPdrsTaskDefinition-1BW7FAS8ZTPN1:1",
-    "lastStatus": "RUNNING",
-    "desiredStatus": "RUNNING",
-    "startedBy": "N/A"
-  }, {
-    "id": "20b71d43-9dda-4778-8af9-b5f463aaf682",
-    "taskDefinition": "cumulus-api-lpdaac-discoverPdrsTaskDefinition-1BW7FAS8ZTPN1:1",
-    "lastStatus": "RUNNING",
-    "desiredStatus": "RUNNING",
-    "startedBy": "N/A"
-  }],
-  "services": [{
-    "serviceName": "cumulus-api-lpdaac-ingestGranulesECSService-OA81E4Y0CV5O",
-    "status": "ACTIVE",
-    "taskDefinition": "cumulus-api-lpdaac-discoverPdrsTaskDefinition-1BW7FAS8ZTPN1:1",
-    "desiredTasks": 1,
-    "runningTasks": 1
-  }, {
-    "serviceName": "cumulus-api-lpdaac-ingestGranulesECSService-OA81E4Y0CV5O",
-    "status": "ACTIVE",
-    "taskDefinition": "cumulus-api-lpdaac-discoverPdrsTaskDefinition-1BW7FAS8ZTPN1:1",
-    "desiredTasks": 1,
-    "runningTasks": 1
-  }, {
-    "serviceName": "cumulus-api-lpdaac-ingestGranulesECSService-OA81E4Y0CV5O",
-    "status": "ACTIVE",
-    "taskDefinition": "cumulus-api-lpdaac-discoverPdrsTaskDefinition-1BW7FAS8ZTPN1:1",
-    "desiredTasks": 1,
-    "runningTasks": 1
-  }]
-});
+  const search = new Search(event, process.env.ResourcesTable);
+  search.query().then(response => cb(null, response.results[0])).catch((e) => {
+    cb(e);
+  });
 }
 
 export function handler(event, context) {
@@ -71,3 +17,6 @@ export function handler(event, context) {
   });
 }
 
+localRun(() => {
+  handler({}, null, (e, r) => console.log(e, r));
+});
