9fdc7fa3f55210437b444fe66b187c23bce6f5bd
Scisco <scisco7@gmail.com>
2017-07-10 17:35:54 -0400

    make archive compatible with new message protocol [wip]

--- lambdas/archive/index.js
+++ lambdas/archive/index.js
@@ -2,10 +2,18 @@
 
 import path from 'path';
 import url from 'url';
+import get from 'lodash.get';
 import log from 'cumulus-common/log';
 import payloadExample from 'cumulus-common/tests/data/payload.json';
-import { S3, invoke } from 'cumulus-common/aws-helpers';
+import { createErrorType } from 'ingest-common/errors';
+import { S3 } from 'cumulus-common/aws-helpers';
 import { localRun } from 'cumulus-common/local';
+
+/**
+ * The error object for when payload files are not provided
+ * @class
+ */
+const PayloadFilesNotFound = createErrorType('PayloadFilesNotFound');
 
 const logDetails = {
   file: 'lambda/archive/index.js',
@@ -13,13 +21,17 @@ const logDetails = {
   type: 'processing'
 };
 
-async function archive(files) {
+async function archive(files, fileDefinitions) {
   // only copy public and protected files
   // keep all the files in the staging folder (need to be moved later on)
   const newFiles = {};
   for (const element of Object.entries(files)) {
     const key = element[0];
-    const file = element[1];
+    const uri = element[1];
+
+    // if the file is on S3, apply archive
+
+
     if (file.stagingFile) {
       let bucket;
       let isPublic = false;
@@ -58,24 +70,33 @@ async function archive(files) {
 }
 
 export function handler(event, context, cb) {
-  const files = event.granuleRecord.files;
-  logDetails.granuleId = event.granuleRecord.granuleId;
-  logDetails.pdrName = event.granuleRecord.pdrName;
-  logDetails.collectionName = event.granuleRecord.collectionName;
+  logDetails.collectionName = get(event, 'collection.id');
+  logDetails.pdrName = get(event, 'payload.pdrName');
+  logDetails.granuleId = get(event, 'payload.granuleId');
+
+  // make sure files are included in the payload
+  const files = get(event, 'payload.files');
+  if (!files) {
+    const err = new PayloadFilesNotFound('Files were not found in the payload');
+    return cb(err);
+  }
+
+  // make sure file definitions are included in the collection meta
+  const fileDefinitions = get(event, 'collection.meta.files');
+  if (!fileDefinitions) {
+    const err = new PayloadFilesNotFound('File definitions were not found in the collection.meta');
+    return cb(err);
+  }
 
   log.info('Started archiving', logDetails);
   log.debug('Started file copy', logDetails);
 
-  archive(files).then((newFiles) => {
+  archive(files, fileDefinitions).then((newFiles) => {
     log.info('All files archived', logDetails);
 
-    event.previousStep = event.nextStep;
-    event.nextStep += 1;
-
-    event.granuleRecord.files = newFiles;
-    return invoke(process.env.dispatcher, event);
+    event.payload.files = newFiles;
+    return event;
   }).then((r) => {
-    log.info('Invoked dispatcher', logDetails);
     cb(null, r);
   }).catch((e) => {
     log.error(e, e.stack, logDetails);
