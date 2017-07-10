85c065c89e88e066537d0e6b70cb3ca2cfd7b256
Scisco <scisco7@gmail.com>
2017-07-10 04:14:44 -0400

    make cmr task compatible with new message protocol

--- lambdas/cmr/index.js
+++ lambdas/cmr/index.js
@@ -1,65 +1,98 @@
 'use strict';
 
-import aws from 'aws-sdk';
 import log from 'cumulus-common/log';
-import { invoke } from 'cumulus-common/aws-helpers';
+import { get } from 'lodash';
+import { S3 } from 'cumulus-common/aws-helpers';
 import { localRun } from 'cumulus-common/local';
 import { ingestGranule } from 'cumulus-common/cmrjs';
+import { createErrorType } from 'ingest-common/errors';
 import payloadExample from 'cumulus-common/tests/data/payload4.json';
 
-const s3 = new aws.S3();
+/**
+ * The error object for when the xml file path is not provided
+ * @class
+ */
+const XmlMetaFileNotFound = createErrorType('XmlMetaFileNotFound');
+
 const logDetails = {
   file: 'lambdas/cmrPush/index.js',
   source: 'pushToCMR',
   type: 'processing'
 };
 
-export async function getMetadata(payload) {
+
+/**
+ * getMetadata
+ *
+ * @param {string} xmlFilePath S3 URI to the xml metadata document
+ * @returns {string} returns stringified xml document downloaded from S3
+ */
+export async function getMetadata(xmlFilePath) {
   // Identify the location of the metadata file,
   // conditional on the name of the collection
-  const filepath = payload.granuleRecord.files['meta-xml'].stagingFile;
+
+  if (!xmlFilePath) {
+    throw new XmlMetaFileNotFound('XML Metadata file not provided');
+  }
 
   // GET the metadata text
   // Currently, only supports files that are stored on S3
-  const parts = filepath.match(/^s3:\/\/(.+?)\/(.+)$/);
-  const obj = await s3.getObject({
-    Bucket: parts[1],
-    Key: parts[2]
-  }).promise();
+  const parts = xmlFilePath.match(/^s3:\/\/(.+?)\/(.+)$/);
+  const obj = await S3.get(parts[1], parts[2]);
 
-  log.info(`Downloaded meta xml file from ${filepath}`, logDetails);
+  log.info(`Downloaded meta xml file from ${xmlFilePath}`, logDetails);
 
   return obj.Body.toString();
 }
 
 
+/**
+ * function for posting xml strings to CMR
+ *
+ * @param {string} xml The strigified xml document that has to be posted to CMR
+ * @param {string} cmrProvider The name of of the CMR provider to be used
+ * @returns {object} CMR's success response which includes the concept-id
+ */
 export async function postToCMR(xml, cmrProvider) {
+  // TODO: pass the username and password provided by the payload
+  // This requires some changes to how cmrjs is initialized and accesses credentials
   log.info('Pushing the metadata to CMR', logDetails);
   return ingestGranule(xml, cmrProvider);
 }
 
 
+/**
+ * Lambda function handler
+ *
+ * @param {object} event Lambda function payload
+ * @param {object} context aws lambda context object
+ * @param {function} cb lambda callback
+ * @returns {object} returns the updated event object
+ */
 export function handler(event, context, cb) {
   try {
-    logDetails.collectionName = event.granuleRecord.collectionName;
-    logDetails.pdrName = event.granuleRecord.pdrName;
-    logDetails.granuleId = event.granuleRecord.granuleId;
-    const cmrProvider = event.granuleRecord.cmrProvider || 'CUMULUS';
+    logDetails.collectionName = get(event, 'collection.id');
+    logDetails.pdrName = get(event, 'payload.pdrName');
+    logDetails.granuleId = get(event, 'payload.granuleId');
+    const cmrProvider = get(event, 'collection.meta.cmrProvider', 'CUMULUS');
+    const xmlFilePath = get(event, 'payload.files[\'meta-xml\']');
+
+    if (!xmlFilePath) {
+      const err = new XmlMetaFileNotFound('Staging XML File is missing');
+      return cb(err);
+    }
 
     log.debug(`Received a new payload: ${JSON.stringify(event)}`, logDetails);
-    getMetadata(event).then(xml => postToCMR(xml, cmrProvider)).then((res) => {
+
+    getMetadata(xmlFilePath).then(xml => postToCMR(xml, cmrProvider)).then((res) => {
       log.info(`successfully posted with this message: ${JSON.stringify(res)}`, logDetails);
 
       // add conceptId to the record
-      event.granuleRecord.cmrLink = 'https://cmr.uat.earthdata.nasa.gov/search/granules.json' +
+      event.payload.cmrLink = 'https://cmr.uat.earthdata.nasa.gov/search/granules.json' +
         `?concept_id=${res.result['concept-id']}`;
-      event.granuleRecord.published = true;
+      event.payload.published = true;
 
-      event.previousStep = event.nextStep;
-      event.nextStep += 1;
-
-      // invoking dispatcher
-      return invoke(process.env.dispatcher, event);
+      return event;
     }).then(res => cb(null, res))
       .catch(e => cb(e));
   }
@@ -67,7 +100,6 @@ export function handler(event, context, cb) {
     log.error(e, logDetails);
     cb(e);
   }
-  // url to check if the CMR is uploaded: https://cmr.uat.earthdata.nasa.gov/search/granules?granule_ur=[granuleUR from xml file]
 }
 
 
---
+++ lambdas/cmr/tests/cmr_test.js
@@ -0,0 +1,45 @@
+'use strict';
+
+import test from 'ava';
+import sinon from 'sinon';
+import { S3 } from 'cumulus-common/aws-helpers';
+import cmrjs from 'cumulus-common/cmrjs';
+import { handler } from '../index';
+import payload from './data/payload.json';
+
+const result = {
+  'concept-id': 'testingtesging'
+};
+
+
+test.before(() => {
+  sinon.stub(S3, 'get').callsFake(() => ({ Body: '<xml></xml>' }));
+  sinon.stub(cmrjs, 'ingestGranule').callsFake(() => ({
+    result
+  }));
+});
+
+test.cb('should succeed with correct payload', (t) => {
+  handler(payload, {}, (e, r) => {
+    t.is(
+      r.payload.cmrLink,
+      `https://cmr.uat.earthdata.nasa.gov/search/granules.json?concept_id=${result['concept-id']}`
+    );
+    t.is(r.payload.published, true);
+    t.end(e);
+  });
+});
+
+test.cb('Should fail if the metadata file uri is missing', (t) => {
+  const newPayload = Object.assign({}, payload);
+  newPayload.payload.files['meta-xml'] = null;
+  handler(newPayload, {}, (e) => {
+    t.truthy(e);
+    t.end();
+  });
+});
+
+test.after(() => {
+  S3.get.restore();
+  cmrjs.ingestGranule.restore();
+});
