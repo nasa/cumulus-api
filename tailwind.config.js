bb70bcde24b388642aec4d0e2f7e1fa73e9cbd91
Juanisa McCoy <juanisa.j.mccoy@nasa.gov>
2023-04-12 16:23:02 -0400

    add styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./website/index.html"],
+  content: ['./website/index.html'],
   theme: {
     screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "976px",
-      xl: "1440px",
-    },
-    colors: {
-      blue: "#2276AC",
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
     },
     fontFamily: {
-      sans: ["Open Sans", "sans-serif"],
+      sans: ['Open Sans', 'sans-serif'],
     },
-    extend: {},
+    extend: {
+      colors: {
+        'blue': '#2276AC',
+        'gray-light': '#F5F6F7',
+        'dark-surface': '#1D1D1D',
+        'dark-blue': '#52BDFF',
+        'dark-highlight': '#EBF7FF'
+      },
+    },
   },
   plugins: [],
 };
